b7b54087d159b64993b1c53c9ae3852fcdd58a6e
Devin Beu <devinbeu@gmail.com>
2024-09-10 09:17:53 -0700

    updated script for linux vm installation

--- installVm-linux.sh
+++ installVm-linux.sh
@@ -1,10 +1,9 @@
 #Check if a vm already is downloaded, if so go ahead, if not download vm 
-
-if ! which multipass > /dev/null 2>&1; 
-then
-  echo 'installing multipass'
-  sudo snap install multipass
-else 
-  echo 'multipass installed'
-fi
 	
+if (which multipass)
+then
+  echo "multipass is already installed"
+else
+  echo "installing mulitpass"
+  # command to install mulitpass
+fi
