24ce96210e07c705b09e72a3d65213869792613f
MonetCarriere <MonetCarriere@yahoo.com>
2024-09-12 08:29:58 -0700

    mac install update. added mos-vm

--- installVm-mac.sh
+++ installVm-mac.sh
@@ -5,3 +5,21 @@ else
   echo "Hold on... installing multipass⏳"
   brew install multipass
 fi
+
+
+
+if (multipass info mos-vm)
+then
+  echo "The VM already exists"
+else
+  echo "Creating mos-vm"
+  multipass launch --name mos-vm
+fi
+
+
+if (multipass info mos-vm)
+then 
+  echo "Launching mos-vm"
+  multipass shell mos-vm
+fi
+  
